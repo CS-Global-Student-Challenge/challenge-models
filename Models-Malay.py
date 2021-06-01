96b15c069d0e4873e44a1f86f722ae35196e9f11
Malay Bhavsar <55908198+Leo-Malay@users.noreply.github.com>
2021-06-01 21:46:53 +0530

    My Models and Training data

---
+++ Models-Malay.py
@@ -0,0 +1,70 @@
+# Modals
+from sklearn.linear_model import LinearRegression, LogisticRegression
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor, AdaBoostClassifier
+# Test train split
+from sklearn.model_selection import train_test_split
+import pandas as pd
+# Error
+from sklearn.metrics import mean_absolute_error, mean_squared_error
+
+# Reading the data.
+fileName = "./train_data.csv"
+data = pd.read_csv(fileName)
+
+# Defing the X and Y.
+X = data[['memory_GB', 'network_log10_MBps',
+          'local_IO_log10_MBps', 'NFS_IO_log10_MBps']]
+Y = data['failed']
+
+# Splitting the data for training and testing.
+train_x, test_x, train_y, test_y = train_test_split(
+    X, Y, train_size=0.8, test_size=0.2, random_state=0)
+
+
+def DecisionTree():
+    myModal = DecisionTreeRegressor()
+    myModal.fit(train_x, train_y)
+    predicated_y = myModal.predict(test_x)
+    return Measure_Error(test_y, predicated_y)
+
+
+def LinearReg():
+    myModal = LinearRegression()
+    myModal.fit(train_x, train_y)
+    predicated_y = myModal.predict(test_x)
+    return Measure_Error(test_y, predicated_y)
+
+
+def LogisticReg():
+    myModal = LogisticRegression()
+    myModal.fit(train_x, train_y)
+    predicated_y = myModal.predict(test_x)
+    return Measure_Error(test_y, predicated_y)
+
+
+def RandomForest():
+    myModal = RandomForestRegressor()
+    myModal.fit(train_x, train_y)
+    predicated_y = myModal.predict(test_x)
+    return Measure_Error(test_y, predicated_y)
+
+
+def AdaBoost():
+    myModal = AdaBoostClassifier()
+    myModal.fit(train_x, train_y)
+    predicated_y = myModal.predict(test_x)
+    return Measure_Error(test_y, predicated_y)
+
+
+def Measure_Error(test_y, predicated_y):
+    return [mean_absolute_error(test_y, predicated_y), mean_squared_error(test_y, predicated_y)]
+
+
+##### Print #####
+print("Mean absolute Error (Lower === Better)")
+print("Decision Tree:", DecisionTree())
+print("Linear Regression:", LinearReg())
+print("Logistic Regression:", LogisticReg())
+print("Random Forest:", RandomForest())
+print("AdaBoost:", AdaBoost())
