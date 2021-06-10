fa679b828d691b203e436abb82426cfe01061b35
Vatsal Parikh <65659649+vatsalparikh07@users.noreply.github.com>
2021-06-10 18:52:52 +0530

    KNeighbour Classifier Added
    KNeighbour Classifier has a better accuracy score than ADABoost

--- Malay-Final-1.py
+++ Malay-Final-1.py
@@ -1,4 +1,6 @@
 import pandas as pd
+from sklearn.metrics import accuracy_score
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import RandomForestRegressor, AdaBoostClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_absolute_error
@@ -16,7 +18,7 @@ train_x, test_x, train_y, test_y = train_test_split(
     X, Y, train_size=0.8, test_size=0.2, random_state=0)
 
 # Model
-myModal = AdaBoostClassifier()
+myModal = KNeighborsClassifier()
 myModal.fit(train_x, train_y)
 predicated_y = myModal.predict(test_x)
 
@@ -28,6 +30,10 @@ print(
 output = pd.DataFrame({'job_id': test_x.index,
                        'failed': predicated_y})
 output.to_csv('model_complete_test.csv', index=False)
+
+score = accuracy_score(test_y, predicated_y)
+print(score)
+
 
 #### Output generates only 4000 rows #####
 #### We need atleast 10000 rows for submission #####
