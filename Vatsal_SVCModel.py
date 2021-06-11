e89560e2ad386708448593a392a5f3650b61df39
996vrushti <78472684+996vrushti@users.noreply.github.com>
2021-06-11 15:12:59 +0530

    Update Vatsal_SVCModel.py

--- Vatsal_SVCModel.py
+++ Vatsal_SVCModel.py
@@ -7,20 +7,21 @@ from sklearn.metrics import mean_absolute_error
 # Reading the file
 data = pd.read_csv('./train_data.csv', index_col="job_id")
 dataTest = pd.read_csv("./test_data_unlabeled.csv", index_col="job_id")
-# Defing the X and Y.
-X = data[['memory_GB', 'network_log10_MBps',
+
+# Defining X and Y
+X = train_data[['memory_GB', 'network_log10_MBps',
           'local_IO_log10_MBps', 'NFS_IO_log10_MBps']]
-Y = data['failed']
-Test_X = dataTest[['memory_GB', 'network_log10_MBps',
+Y = train_data['failed']
+test_X = test_data[['memory_GB', 'network_log10_MBps',
                    'local_IO_log10_MBps', 'NFS_IO_log10_MBps']]
 
 # Model
-myModal = SVC(gamma=2.25, C=1)
-myModal.fit(X, Y)
-predicated_y = myModal.predict(Test_X)
+myModel = SVC(gamma=2.25, C=1)
+myModel.fit(X, Y)
+predicted_y = myModel.predict(test_X)
 
 # Writing to csv
-output = pd.DataFrame({'job_d': Test_X.index,
-                       'failed': predicated_y})
+output = pd.DataFrame({'job_id': test_X.index,
+                       'failed': predicted_y})
 output.to_csv('model_complete_test.csv', index=False)
 
