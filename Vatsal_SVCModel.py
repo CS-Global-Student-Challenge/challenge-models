e5d40cf933dcff18e44f2cde964efe352a585f60
996vrushti <78472684+996vrushti@users.noreply.github.com>
2021-06-11 15:08:36 +0530

    Update Vatsal_SVCModel.py

--- Vatsal_SVCModel.py
+++ Vatsal_SVCModel.py
@@ -1,17 +1,7 @@
 import pandas as pd
 from sklearn.metrics import accuracy_score
-from sklearn.neighbors import KNeighborsClassifier,KNeighborsTransformer,NearestCentroid
-from sklearn.semi_supervised import SelfTrainingClassifier
 from sklearn.svm import SVC
-from sklearn.ensemble import RandomForestRegressor, AdaBoostClassifier, RandomForestClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_absolute_error
-from sklearn.neural_network import MLPClassifier
-from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
-from sklearn.gaussian_process import GaussianProcessClassifier
-from sklearn.gaussian_process.kernels import RBF
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.naive_bayes import GaussianNB
 
 # Reading the file
 data = pd.read_csv('./train_data.csv', index_col="job_id")
