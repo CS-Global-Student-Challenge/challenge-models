51ccd3a7dea77dceea59ce5964940765827b04c4
Vatsal Parikh <65659649+vatsalparikh07@users.noreply.github.com>
2021-06-11 15:09:26 +0530

    Update Vatsal_SVCModel.py

--- Vatsal_SVCModel.py
+++ Vatsal_SVCModel.py
@@ -1,7 +1,17 @@
 import pandas as pd
 from sklearn.metrics import accuracy_score
+from sklearn.neighbors import KNeighborsClassifier,KNeighborsTransformer,NearestCentroid
+from sklearn.semi_supervised import SelfTrainingClassifier
 from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestRegressor, AdaBoostClassifier, RandomForestClassifier
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_absolute_error
+from sklearn.neural_network import MLPClassifier
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
+from sklearn.gaussian_process import GaussianProcessClassifier
+from sklearn.gaussian_process.kernels import RBF
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.naive_bayes import GaussianNB
 
 # Reading the file
 data = pd.read_csv('./train_data.csv', index_col="job_id")
@@ -14,7 +24,7 @@ Test_X = dataTest[['memory_GB', 'network_log10_MBps',
                    'local_IO_log10_MBps', 'NFS_IO_log10_MBps']]
 
 # Model
-myModal = KNeighborsClassifier(n_neighbors=4)
+myModal = SVC(gamma=2.25, C=1)
 myModal.fit(X, Y)
 predicated_y = myModal.predict(Test_X)
 
