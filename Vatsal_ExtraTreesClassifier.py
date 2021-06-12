65b06b130989aaa3afe034ae51731f4e3c4e3c67
Vatsal Parikh <65659649+vatsalparikh07@users.noreply.github.com>
2021-06-13 01:10:09 +0530

    ExtraTreeClassifier Model score = 0.69840

---
+++ Vatsal_ExtraTreesClassifier.py
@@ -0,0 +1,27 @@
+import pandas as pd
+from sklearn.metrics import accuracy_score
+from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_absolute_error
+from sklearn.ensemble import ExtraTreesClassifier
+
+# Reading the file
+train_data = pd.read_csv('./train_data.csv', index_col="job_id")
+test_data = pd.read_csv("./test_data_unlabeled.csv", index_col="job_id")
+
+# Defining X and Y
+X = train_data[['memory_GB', 'network_log10_MBps',
+          'local_IO_log10_MBps', 'NFS_IO_log10_MBps']]
+Y = train_data['failed']
+test_X = test_data[['memory_GB', 'network_log10_MBps',
+                   'local_IO_log10_MBps', 'NFS_IO_log10_MBps']]
+
+# Model
+myModel = ExtraTreesClassifier(n_estimators=200, random_state=0,min_samples_split = 5)
+myModel.fit(X, Y)
+predicted_y = myModel.predict(test_X)
+
+# Writing to csv
+output = pd.DataFrame({'job_id': test_X.index,
+                       'failed': predicted_y})
+output.to_csv('model_complete_test.csv', index=False)
